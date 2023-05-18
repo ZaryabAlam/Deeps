fc6d96744e56af62e67ce8927fe075df94fe046e
ZaryabAlam <zaryabalam97@gmail.com>
2023-05-18 12:21:56 +0500

    Project Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+//test
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
